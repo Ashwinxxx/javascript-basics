707fda4b979d04fbabbe398c2d98470af2cbe46f
Ashwinxxx <117080163+Ashwinxxx@users.noreply.github.com>
2024-06-19 14:26:14 +0000

    data types conversion

---
+++ 01_basics/test.js
@@ -0,0 +1,13 @@
+
+//let score = true
+//console.log(typeof(score))
+//let //valueinNumber=Number(score)
+//console.log(typeof valueinNumber)
+//console.log(valueinNumber)
+//let isloggedin=""
+//let booleanIsloggedin=Boolean(isloggedin)
+//console.log(booleanIsloggedin);
+let a=33
+let Stringno=String(a)
+console.log(Stringno)
+console.log(typeof Stringno)
