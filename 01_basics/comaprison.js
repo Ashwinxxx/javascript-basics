2d1abd7addf98d88691ecae1bf3768bcabd52170
Ashwinxxx <117080163+Ashwinxxx@users.noreply.github.com>
2024-06-19 17:21:31 +0000

    summary of datatypes

---
+++ 01_basics/comaprison.js
@@ -0,0 +1,24 @@
+//console.log("2">1)
+//console.log("02">1)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+//===
+console.log("2" ===2)
+//primitive null,boolean,string,undefined,symbol,BigInt
+//reference  array objects,functions
+const heros=["a", "b","c"];
+let myobj = {
+    name :"a",
+    age: 22,
+}
+const myfunction=function(){
+    console/log("hello world")
+}
+console.log(typeof bigNumber)
+console.log(typeof heros)
+//  null object
+//object function
+//function objectfunction
+
+console.log(typeof myfunction)
