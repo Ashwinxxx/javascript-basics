6ba829ed629e104d9485f9365ca7d8fd4b860726
Ashwinxxx <117080163+Ashwinxxx@users.noreply.github.com>
2024-06-20 09:38:49 +0000

    dates

---
+++ 01_basics/date.js
@@ -0,0 +1,14 @@
+let m=new Date()
+//console.log(m.toString());
+//console.log(typeof m)
+let n=new Date("05-05-2024")
+//console.log(n.toLocaleString());
+let timestamp = Date.now()
+//console.log(timestamp);
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+//console.log(newDate);
+//console.log(newDate.getDate());
+//console.log(newDate.getDay());
+
