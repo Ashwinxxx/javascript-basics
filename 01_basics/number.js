50c932c17ce3e6f742a9b4b3c4d016e3bdff0f3f
Ashwinxxx <117080163+Ashwinxxx@users.noreply.github.com>
2024-06-20 09:24:56 +0000

    numbers

--- 01_basics/number.js
+++ 01_basics/number.js
@@ -1,17 +1,17 @@
-const score=400
-const balance = new Number(100)
+//const score=400
+//const balance = new Number(100)
 //console.log(balance);
 //console.log(balance.toString().length);
 //console.log(balance.toFixed(1));
-const othermo=1122.888
+//const othermo=1122.888
 //console.log(othermo.toPrecision(4));
-const hun=100000
+//const hun=100000
 //console.log(hun.toLocaleString('en-IN'));
 // Maths Library js
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.random()*10)+1;
-const min=10
-const max=20
-console.log(Math.floor(Math.random() * (max-min+1) +min));
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));
+//console.log(Math.random()*10)+1;
+//const min=10
+//const max=20
+//console.log(Math.floor(Math.random() * (max-min+1) +min));
